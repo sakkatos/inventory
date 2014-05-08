2f8e5101326158837fe3007d8e229f1470dc2093
Toy story <toypinta@gmail.com>
2014-05-08 17:04:31 +0700

    create all of OrderItem

---
+++ src/main/java/th/co/geniustree/inventory/repo/OrderItemRepo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package th.co.geniustree.inventory.repo;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import th.co.geniustree.inventory.model.OrderItem;
+
+/**
+ *
+ * @author toy
+ */
+public interface OrderItemRepo extends JpaRepository<OrderItem, Integer>{
+    public List<OrderItem> findByNameLike(String orderItem);
+    
+}
---
+++ src/main/java/th/co/geniustree/inventory/service/impl/OrderItemServiceImpl.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package th.co.geniustree.inventory.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import th.co.geniustree.inventory.model.OrderItem;
+import th.co.geniustree.inventory.repo.OrderItemRepo;
+import th.co.geniustree.inventory.service.OrderItemService;
+
+/**
+ *
+ * @author toy
+ */
+@Service
+@Transactional(propagation = Propagation.REQUIRED)
+public class OrderItemServiceImpl implements OrderItemService{
+
+    @Autowired
+    private OrderItemRepo orderItemRepo;
+    
+    @Override
+    public OrderItem save(OrderItem orderItem) {
+        return orderItemRepo.save(orderItem);
+    }
+
+    @Override
+    public void deleteByName(OrderItem orderItem) {
+        orderItemRepo.delete(orderItem);
+    }
+
+    @Override
+    public List<OrderItem> findAll() {
+        return orderItemRepo.findAll();
+    }
+
+    @Override
+    public List<OrderItem> findByNameLike(String orderItem) {
+        return orderItemRepo.findByNameLike(orderItem);
+    }
+
+    @Override
+    public Page findAll(PageRequest pageRequest) {
+        return orderItemRepo.findAll(pageRequest);
+    }
+
+    @Override
+    public Page findAllPage(PageRequest pageRequest) {
+        return null;
+    }
+    
+}
