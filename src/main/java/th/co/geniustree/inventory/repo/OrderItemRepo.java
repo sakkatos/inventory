621bbce2f6138f39d4c1f9f7b59950283fd576d3
Nook <Nook@Nook-Labtop>
2014-05-30 10:57:14 +0700

    Revert "edit order item and purchase"
    This reverts commit ec40934fafb702ca8c102d210f7cab3da46341d9.

--- src/main/java/th/co/geniustree/inventory/repo/OrderItemRepo.java
+++ src/main/java/th/co/geniustree/inventory/repo/OrderItemRepo.java
@@ -8,9 +8,7 @@ package th.co.geniustree.inventory.repo;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import th.co.geniustree.inventory.model.OrderItem;
-import th.co.geniustree.inventory.model.PurchaseOrder;
 
 /**
  *
@@ -18,8 +16,5 @@ import th.co.geniustree.inventory.model.PurchaseOrder;
  */
 public interface OrderItemRepo extends JpaRepository<OrderItem, Integer>{
     public List<OrderItem> findByIdLike(Integer orderItem);
-    
-     @Query("SELECT oi FROM OrderItem oi WHERE oi.purchaseOrder = ?1")
-    public List<OrderItem> findOrderItemByPurchaseOrder(PurchaseOrder purchaseOrder);
     
 }
